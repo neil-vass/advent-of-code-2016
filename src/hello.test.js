c6e4b86c06e5472a575fcffcb6c14cc631f5006d
Neil Vass <neil.vass+github@gmail.com>
2024-10-29 21:22:58 +0000

    TypeScript and Node set up, test framework in place.

---
+++ src/hello.test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vitest_1 = require("vitest");
+(0, vitest_1.describe)("Hello", () => {
+    (0, vitest_1.it)("Runs a test", () => {
+        (0, vitest_1.expect)(1).toBe(1);
+    });
+});
---
+++ src/hello.test.ts
@@ -0,0 +1,7 @@
+import {expect, describe, it} from "vitest";
+
+describe("Hello", () => {
+    it("Runs a test", () => {
+        expect(1).toBe(1);
+    });
+});
