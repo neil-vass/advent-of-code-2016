bf75cb205f0907f9a535bdbc49f2d1e4b4eabba3
Neil Vass <neil.vass+github@gmail.com>
2024-11-25 14:22:11 +0000

    Set up heuristic

--- src/day11/day11.test.ts
+++ src/day11/day11.test.ts
@@ -1,5 +1,5 @@
 import {expect, describe, it, beforeEach} from "vitest";
-import {Facility, Floor, Item, parseFloor} from "./day11.js";
+import {Explorer, Facility, Floor, Item, parseFloor} from "./day11.js";
 import {Sequence} from "generator-sequences";
 
 
@@ -13,12 +13,52 @@ describe("Part 1", () => {
             ]));
     });
 
-    it("Solves a very basic puzzle", async () => {
+    it("Serilializes for easy checking of whether we've seen a state before", async () => {
         const input = new Sequence([
             "The first floor contains a hydrogen-compatible microchip.",
             "The second floor contains a hydrogen generator."
         ]);
-        const facility = Facility.buildFromDescription(input);
-
+        const facility = await Facility.buildFromDescription(input);
+        expect(facility.serialize()).toBe(
+            '{"floors":[{"items":["hydrogen microchip"]},{"items":["hydrogen generator"]}],"elevatorFloor":0}');
     });
+
+    it("Deserializes to come back and explore a state further", async () => {
+        // Same string as created by .serialize() in previous test.
+        const serial = '{"floors":[{"items":["hydrogen microchip"]},{"items":["hydrogen generator"]}],"elevatorFloor":0}'
+
+        const facility = Facility.deserialize(serial);
+        expect(facility.elevatorFloor).toBe(0);
+        expect(facility.floors.length).toBe(2);
+        expect(facility.floors[0].items).toStrictEqual(new Set([
+            new Item("hydrogen", "microchip")
+        ]));
+    });
+
+    it("Calculates single-step heuristic", async () => {
+        // You can complete this in one move.
+        // We know you could fit 2 items in the elevator at once, so each
+        // single thing you move counts as 0.5 steps for scoring.
+        const serial = '{"floors":[{"items":["hydrogen microchip"]},{"items":["hydrogen generator"]}],"elevatorFloor":0}'
+
+        expect(Explorer.heuristic(serial, "")).toBeCloseTo(0.5);
+    });
+
+    it("Calculates simple heuristic", async () => {
+        // The true best case from the puzzle example: 11 moves.
+        // Our heuristic will be overconfident, but still useful.
+        // Heuristic assumes: grab both microchips (2 will fit in the elevator),
+        // and take them straight up to the top - it doesn't consider needing to
+        // have the correct generators - for a total of 3 steps.
+        const input = new Sequence([
+            "The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.",
+            "The second floor contains a hydrogen generator.",
+            "The third floor contains a lithium generator.",
+            "The fourth floor contains nothing relevant.",
+        ]);
+
+        const facility = await Facility.buildFromDescription(input);
+        expect(Explorer.heuristic(facility.serialize(), "")).toBeCloseTo(3);
+    });
+
 });
--- src/day11/day11.ts
+++ src/day11/day11.ts
@@ -1,9 +1,12 @@
 import exp from "node:constants";
 import {Sequence} from "generator-sequences";
+import {WeightedGraph} from "../utils/graphSearch.js";
 
 const GENERATOR = "generator";
 const MICROCHIP = "microchip";
 export type TechType = typeof GENERATOR | typeof MICROCHIP;
+
+export type SerializedFacility = string;
 
 export function parseFloor(line: string) {
     const floor = new Floor();
@@ -22,10 +25,19 @@ export class Item {
 
 export class Floor {
     readonly items = new Set<Item>();
+
+    // Sets don't show their contents when serialized by JSON.stringify.
+    // This method will get called, gives the items as an array instead.
+    // We want the same Set to give the same JSON every time, so we
+    // recognize whether we've seen it before, so it needs to be sorted.
+    toJSON() {
+        const description = [...this.items].map(i => `${i.element} ${i.tech}`);
+        return { "items": description.sort() }
+    }
 }
 
 export class Facility {
-    private elevatorFloor = 0;
+    elevatorFloor = 0;
     private constructor(readonly floors: Array<Floor>) {}
 
     static async buildFromDescription(lines: Sequence<string>) {
@@ -37,9 +49,63 @@ export class Facility {
         return new Facility(floors);
     }
 
+    serialize(): SerializedFacility {
+        return JSON.stringify(this);
+    }
+
+    static deserialize(serial: SerializedFacility) {
+        // '{"floors":[{"items":["hydrogen microchip"]},{"items":["hydrogen generator"]}],"elevatorFloor":0}'
+        const data = JSON.parse(serial);
+
+        const floors = new Array<Floor>();
+        for (const floorData of data.floors) {
+            const floor = new Floor();
+            for (const itemData of floorData.items) {
+                const [element, tech] = itemData.split(" ");
+                floor.items.add(new Item(element, tech as TechType));
+            }
+            floors.push(floor);
+        }
+
+        const facility = new Facility(floors);
+        facility.elevatorFloor = data.elevatorFloor;
+        return facility;
+    }
+
+
     // You have possible steps, some of which are dead ends.
     // When a path's taking you _away_ from the state you want, weight against it.
     //
+}
+
+// Let's go exploring.
+export const Explorer: WeightedGraph<SerializedFacility> = {
+    // "to" is ignored here; rather than a specific destination, we want some
+    // conditions met and don't care about others.
+    heuristic(from: SerializedFacility, to: SerializedFacility): number {
+
+        // Win condition: all microchips on top floor, don't care where
+        // anything else is (generators, elevator). Min moves from here to
+        // there: you can move two items at a time, so let's say 0.5 "cost"
+        // for each floor away from the top each microchip is.
+        let score = 0;
+
+        const currentState = Facility.deserialize(from);
+        const topFloorNum = currentState.floors.length - 1;
+        if (topFloorNum < 1) return score;
+
+        for (let currFloor = 0; currFloor < topFloorNum; currFloor++) {
+            const items =  [...currentState.floors[currFloor].items];
+            const chips = items.filter(item => item.tech === MICROCHIP).length;
+            score += 0.5 * chips * (topFloorNum - currFloor);
+        }
+
+        return score;
+    },
+
+    neighbours(currentNode: SerializedFacility): Iterable<{ node: SerializedFacility; cost: number }> {
+        return undefined;
+    }
 }
 
 // If this script was invoked directly on the command line:
