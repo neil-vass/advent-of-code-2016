a2ce56d19270f6f7e2a09cf17574005a82cca90e
Neil Vass <neil.vass+github@gmail.com>
2024-11-26 16:15:33 +0000

    Day 12 setup

---
+++ src/day12/day12.test.ts
@@ -0,0 +1,8 @@
+import {expect, describe, it} from "vitest";
+import {fn} from "./day12.js";
+
+describe("#fn", () => {
+    it("runs", () => {
+        expect(fn("")).toBe("a real test");
+    });
+});
