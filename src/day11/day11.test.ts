9d460bb69ff37e89d051634495915d29d07a0927
Neil Vass <neil.vass+github@gmail.com>
2024-11-26 11:33:12 +0000

    Day 11 part 1 done

--- src/day11/day11.test.ts
+++ src/day11/day11.test.ts
@@ -1,16 +1,17 @@
 import {expect, describe, it, beforeEach} from "vitest";
-import {Explorer, Facility, Floor, GOAL_CONDITION, Item, parseFloor} from "./day11.js";
+import {Explorer, Facility, Floor, GOAL_CONDITION, Item, onesAndTwos, parseFloor, solvePart1} from "./day11.js";
 import {Sequence} from "generator-sequences";
+import {isNumberObject} from "node:util/types";
 
 
 describe("Part 1", () => {
     it("Creates a floor", () => {
         const line = "The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.";
         const floor = parseFloor(line);
-        expect(floor.items).toStrictEqual(new Set([
+        expect(floor.items).toStrictEqual([
             new Item("hydrogen", "microchip"),
             new Item("lithium", "microchip")
-            ]));
+            ]);
     });
 
     it("Serializes for easy checking of whether we've seen a state before", async () => {
@@ -25,23 +26,23 @@ describe("Part 1", () => {
 
     it("Serializes to the general GOAL_CONDITION response if goal is reached", async () => {
         const input = new Sequence([
-            "The first floor contains a hydrogen generator.",
-            "The second floor contains a hydrogen-compatible microchip and a lithium-compatible microchip."
+            "The first floor contains nothing.",
+            "The second floor contains a hydrogen-compatible microchip and a hydrogen generator."
         ]);
         const facility = await Facility.buildFromDescription(input);
         expect(facility.serializeToCheckForGoalCondition()).toBe(GOAL_CONDITION);
     });
 
     it("Deserializes to come back and explore a state further", async () => {
-        // Same string as created by .serialize() in previous test.
+        // Same string as created by serializing in a previous test.
         const serial = '{"floors":[{"items":["hydrogen microchip"]},{"items":["hydrogen generator"]}],"elevatorFloor":0}'
 
         const facility = Facility.deserialize(serial);
         expect(facility.elevatorFloor).toBe(0);
         expect(facility.floors.length).toBe(2);
-        expect(facility.floors[0].items).toStrictEqual(new Set([
+        expect(facility.floors[0].items).toStrictEqual([
             new Item("hydrogen", "microchip")
-        ]));
+        ]);
     });
 
 
@@ -51,7 +52,7 @@ describe("Part 1", () => {
         // single thing you move counts as 0.5 steps for scoring.
         const serial = '{"floors":[{"items":["hydrogen microchip"]},{"items":["hydrogen generator"]}],"elevatorFloor":0}'
 
-        expect(Explorer.heuristic(serial, "")).toBeCloseTo(0.5);
+        expect(Explorer.heuristic(serial, GOAL_CONDITION)).toBeCloseTo(0.5);
     });
 
     it("Calculates simple heuristic", async () => {
@@ -68,7 +69,71 @@ describe("Part 1", () => {
         ]);
 
         const facility = await Facility.buildFromDescription(input);
-        expect(Explorer.heuristic(facility.serializeToCheckForGoalCondition(), "")).toBeCloseTo(3);
+        expect(Explorer.heuristic(facility.serializeToCheckForGoalCondition(), GOAL_CONDITION)).toBeCloseTo(3);
     });
 
+    it("Returns next step when there's only one choice", async () => {
+        // We can complete this in one step.
+        const serial = '{"floors":[{"items":["hydrogen microchip"]},{"items":["hydrogen generator"]}],"elevatorFloor":0}'
+        let neighboursCount = 0;
+        for (const neighbour of Explorer.neighbours(serial)) {
+            expect(neighbour).toStrictEqual({ node: GOAL_CONDITION, cost: 1});
+            neighboursCount++;
+        }
+        expect(neighboursCount).toBe(1);
+    });
+
+    it("Returns all one- and two- item combinations from a list", async () => {
+        // Works on any array.
+        const input = ["a", "b", "c"];
+        expect([...onesAndTwos(input.length)]).toStrictEqual([
+            [ 0 ],
+            [ 1, 0 ],
+            [ 2, 0 ],
+            [ 1 ],
+            [ 2, 1 ],
+            [ 2 ]
+        ]);
+    });
+
+    it("Moves items to requested floor", async () => {
+        const input = new Sequence([
+            "The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.",
+            "The second floor contains a hydrogen generator.",
+            "The third floor contains a lithium generator.",
+            "The fourth floor contains nothing relevant.",
+        ]);
+        const facility = await Facility.buildFromDescription(input);
+        facility.move([0, 1], 1);
+        expect(facility.floors[0].items.length).toBe(0);
+        expect(facility.floors[1].items.length).toBe(3);
+        expect(facility.elevatorFloor).toBe(1);
+        expect(facility.isValid()).toBe(false);
+    });
+
+    it("Solves a single-step puzzle", async () => {
+        const input = new Sequence([
+            "The first floor contains a hydrogen-compatible microchip.",
+            "The second floor contains a hydrogen generator."
+        ]);
+        expect(await solvePart1(input)).toBe(1);
+    });
+
+    it("Microchips are protected from other generators when their compatible one is present", async () => {
+        const input = new Sequence([
+            "The first floor contains a hydrogen-compatible microchip.",
+            "The second floor contains a hydrogen generator and a lithium generator."
+        ]);
+        expect(await solvePart1(input)).toBe(1);
+    });
+
+    it("Solves example from puzzle description", async () => {
+        const input = new Sequence([
+            "The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.",
+            "The second floor contains a hydrogen generator.",
+            "The third floor contains a lithium generator.",
+            "The fourth floor contains nothing relevant.",
+        ]);
+        expect(await solvePart1(input)).toBe(11);
+    });
 });
--- src/day11/day11.ts
+++ src/day11/day11.ts
@@ -1,6 +1,6 @@
 import exp from "node:constants";
-import {Sequence} from "generator-sequences";
-import {WeightedGraph} from "../utils/graphSearch.js";
+import {linesFromFile, Sequence} from "generator-sequences";
+import {A_starSearch, WeightedGraph} from "../utils/graphSearch.js";
 
 const GENERATOR = "generator";
 const MICROCHIP = "microchip";
@@ -14,7 +14,7 @@ export function parseFloor(line: string) {
     const matches = line.matchAll(/ ([a-z]+)(?:-compatible)? (generator|microchip)/g);
     for (const m of matches) {
         const [, element, tech] = m;
-        floor.items.add(new Item(element, tech as TechType));
+        floor.items.push(new Item(element, tech as TechType));
     }
     return floor;
 }
@@ -25,15 +25,28 @@ export class Item {
 }
 
 export class Floor {
-    readonly items = new Set<Item>();
+    readonly items = new Array<Item>();
 
-    // Sets don't show their contents when serialized by JSON.stringify.
-    // This method will get called, gives the items as an array instead.
-    // We want the same Set to give the same JSON every time, so we
+    // This method will get called when serialized by JSON.stringify.
+    // We want the same set to give the same JSON every time, so we
     // recognize whether we've seen it before, so it needs to be sorted.
     toJSON() {
-        const description = [...this.items].map(i => `${i.element} ${i.tech}`);
+        const description = this.items.map(i => `${i.element} ${i.tech}`);
         return { "items": description.sort() }
+    }
+
+    // True if we're not irradiating any microchips here.
+    // It's fine if: there are no generators at all, or if each microchip
+    // has its matching generator to protect it.
+    isValid() {
+        const generators = this.items.filter(i => i.tech === GENERATOR);
+        if (generators.length === 0) return true;
+
+        const microchips = this.items.filter(i => i.tech === MICROCHIP);
+        for (const m of microchips) {
+            if (generators.every(g => g.element !== m.element)) return false;
+        }
+        return true;
     }
 }
 
@@ -50,14 +63,13 @@ export class Facility {
         return new Facility(floors);
     }
 
-
     serializeToCheckForGoalCondition(): SerializedFacility {
-        // If all the microchips are on the top floor, we've met the goal.
-        const itemsNotAtTop = this.floors.slice(0, -1).flatMap(f => [...f.items]);
-        if (itemsNotAtTop.some(item => item.tech === MICROCHIP)) {
-            return JSON.stringify(this);
-        } else {
+        // If all the items are on the top floor, we've met the goal.
+        const itemsNotAtTop = this.floors.slice(0, -1).flatMap(f => f.items);
+        if (itemsNotAtTop.length === 0) {
             return GOAL_CONDITION;
+        } else {
+            return JSON.stringify(this);
         }
     }
 
@@ -70,7 +82,7 @@ export class Facility {
             const floor = new Floor();
             for (const itemData of floorData.items) {
                 const [element, tech] = itemData.split(" ");
-                floor.items.add(new Item(element, tech as TechType));
+                floor.items.push(new Item(element, tech as TechType));
             }
             floors.push(floor);
         }
@@ -80,10 +92,23 @@ export class Facility {
         return facility;
     }
 
+    numItemsOnCurrentFloor() {
+        return this.floors[this.elevatorFloor].items.length;
+    }
 
-    // You have possible steps, some of which are dead ends.
-    // When a path's taking you _away_ from the state you want, weight against it.
-    //
+    move(itemsToMove: Array<number>, floorIdx: number) {
+        // Sort indexes descending, or we'll lose track as we remove things.
+        itemsToMove = [...itemsToMove].sort((a,b) => b-a);
+        for (const itemIdx of itemsToMove) {
+            const [item] = this.floors[this.elevatorFloor].items.splice(itemIdx, 1);
+            this.floors[floorIdx].items.push(item);
+        }
+        this.elevatorFloor = floorIdx;
+    }
+
+    isValid() {
+        return this.floors.every(f => f.isValid());
+    }
 }
 
 // Let's go exploring.
@@ -92,11 +117,11 @@ export const Explorer: WeightedGraph<SerializedFacility> = {
     // "to" is ignored here; rather than a specific destination, we want some
     // conditions met and don't care about others.
     heuristic(from: SerializedFacility, to: SerializedFacility): number {
-
         // Win condition: all microchips on top floor, don't care where
         // anything else is (generators, elevator). Min moves from here to
         // there: you can move two items at a time, so let's say 0.5 "cost"
         // for each floor away from the top each microchip is.
+        if (from === GOAL_CONDITION) return 0;
         let score = 0;
 
         const currentState = Facility.deserialize(from);
@@ -104,7 +129,7 @@ export const Explorer: WeightedGraph<SerializedFacility> = {
         if (topFloorNum < 1) return score;
 
         for (let currFloor = 0; currFloor < topFloorNum; currFloor++) {
-            const items =  [...currentState.floors[currFloor].items];
+            const items =  currentState.floors[currFloor].items;
             const chips = items.filter(item => item.tech === MICROCHIP).length;
             score += 0.5 * chips * (topFloorNum - currFloor);
         }
@@ -113,13 +138,57 @@ export const Explorer: WeightedGraph<SerializedFacility> = {
     },
 
     // Where can we get to from here, in one step?
-    neighbours(currentNode: SerializedFacility): Iterable<{ node: SerializedFacility; cost: number }> {
-        return undefined;
+    *neighbours(currentNode: SerializedFacility): Iterable<{ node: SerializedFacility; cost: number }> {
+        let currentState = Facility.deserialize(currentNode);
+
+        const floorsWeCanMoveToIdx = new Array<number>();
+        if (currentState.elevatorFloor > 0) {
+            floorsWeCanMoveToIdx.push(currentState.elevatorFloor-1);
+        }
+        if (currentState.elevatorFloor < currentState.floors.length-1) {
+            floorsWeCanMoveToIdx.push(currentState.elevatorFloor+1);
+        }
+
+        for (const itemsToMove of onesAndTwos(currentState.numItemsOnCurrentFloor())) {
+            for (const floorIdx of floorsWeCanMoveToIdx) {
+                currentState.move(itemsToMove, floorIdx);
+                if (currentState.isValid()) {
+                    yield {
+                        node: currentState.serializeToCheckForGoalCondition(),
+                        cost: 1
+                    };
+                }
+                // Reset changes and keep looking for neighbours.
+                currentState = Facility.deserialize(currentNode);
+            }
+        }
+    }
+}
+
+export function *onesAndTwos(itemCount: number) {
+    for(let i=0; i< itemCount; i++) {
+        yield [i];
+        for (let j=i+1; j< itemCount; j++) {
+            yield [j, i];
+        }
+    }
+}
+
+export async function solvePart1(input: Sequence<string>) {
+    const facility = await Facility.buildFromDescription(input);
+    const initialCondition = facility.serializeToCheckForGoalCondition();
+    const searchResult = A_starSearch(Explorer, initialCondition, GOAL_CONDITION);
+    const bestPathToGoal = searchResult.get(GOAL_CONDITION);
+
+    if (bestPathToGoal === undefined) {
+        throw new Error(`No solution found`);
+    } else {
+        return bestPathToGoal.costSoFar;
     }
 }
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
     const filepath = `${import.meta.dirname}/day11.input.txt`;
-    console.log((filepath));
+    console.log(await solvePart1(linesFromFile(filepath)));
 }
