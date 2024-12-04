1833f56ecc7176f480aa901292f04ee017cfc23c
Neil Vass <neil.vass+github@gmail.com>
2024-12-04 22:32:12 +0000

    Day 22 done

--- src/day22/day22.test.ts
+++ src/day22/day22.test.ts
@@ -1,5 +1,5 @@
 import {describe, expect, it} from "vitest";
-import {parseNode, solvePart1} from "./day22.js";
+import {parseNode, solvePart1, solvePart2} from "./day22.js";
 import {Sequence} from "generator-sequences";
 
 describe("Part 1", () => {
@@ -18,5 +18,23 @@ describe("Part 1", () => {
             "/dev/grid/node-x2-y0     20T   18T   2T   69%",
         ]);
         expect(await solvePart1(lines)).toBe(4);
+    })
+});
+
+describe("Part 2", () => {
+    it("Draws diagram", async () => {
+        const lines = new Sequence([
+            "Filesystem            Size  Used  Avail  Use%",
+            "/dev/grid/node-x0-y0   10T    8T     2T   80%",
+            "/dev/grid/node-x0-y1   11T    6T     5T   54%",
+            "/dev/grid/node-x0-y2   32T   28T     4T   87%",
+            "/dev/grid/node-x1-y0    9T    7T     2T   77%",
+            "/dev/grid/node-x1-y1    8T    0T     8T    0%",
+            "/dev/grid/node-x1-y2   11T    7T     4T   63%",
+            "/dev/grid/node-x2-y0   10T    6T     4T   60%",
+            "/dev/grid/node-x2-y1    9T    8T     1T   88%",
+            "/dev/grid/node-x2-y2    9T    6T     3T   66%",
+        ]);
+        expect(await solvePart2(lines)).toBe("Next: paste into a spreadsheet");
     })
 });
--- src/day22/day22.ts
+++ src/day22/day22.ts
@@ -1,12 +1,21 @@
 import {linesFromFile, Sequence} from "generator-sequences";
-import {av} from "vitest/dist/chunks/reporters.D7Jzd9GS.js";
 
-export type StorageNode = {name: string, used: number, avail: number};
+export type StorageNode = {name: string, size: number, used: number, avail: number};
+
+function posFromName(name: string) {
+    const m = name.match(/^node-x(\d+)-y(\d+)$/);
+    if (!m) throw new Error(`Unrecognized format: ${name}`);
+    return {x: +m[1], y: +m[2]};
+}
+
+function nameFromPos(x: number, y: number) {
+    return `node-x${x}-y${y}`;
+}
 
 export function parseNode(line: string): StorageNode {
-    const m = line.match(/^\/dev\/grid\/(node-x\d+-y\d+)\s+\d+T\s+(\d+)T\s+(\d+)T\s+\d+%$/);
+    const m = line.match(/^\/dev\/grid\/(node-x\d+-y\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+\d+%$/);
     if (!m) throw new Error(`Unrecognized format: ${line}`);
-    return {name: m[1], used: +m[2], avail: +m[3]};
+    return {name: m[1], size: +m[2], used: +m[3], avail: +m[4]};
 }
 
 export async function solvePart1(lines: Sequence<string>) {
@@ -29,9 +38,34 @@ export async function solvePart1(lines: Sequence<string>) {
     return pairCount;
 }
 
+
+export async function solvePart2(lines: Sequence<string>) {
+    const nodes = lines.filter(ln => ln.startsWith("/dev/")).map(parseNode);
+
+    const nodeMap = new Map<string, StorageNode>();
+    let xMax = 0;
+    let yMax = 0;
+    for await (const node of nodes) {
+        nodeMap.set(node.name, node);
+        const {x,y} = posFromName(node.name);
+        xMax = Math.max(x, xMax);
+        yMax = Math.max(y, yMax);
+    }
+
+    for (let y=0; y <= yMax; y++) {
+        let row = "";
+        for (let x=0; x <= xMax; x++) {
+            const node = nodeMap.get(nameFromPos(x,y))!;
+            row += node.used + "/" + node.size;
+            if (x < xMax) row += "\t";
+        }
+        console.log(row);
+    }
+    return "Next: paste into a spreadsheet"
+}
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
     const filepath = `${import.meta.dirname}/day22.input.txt`;
-    console.log(await solvePart1(linesFromFile(filepath)));
+    console.log(await solvePart2(linesFromFile(filepath)));
 }
 
